100e70a912c9972fc4abd3263def6c01e0804290
venkatesh-boddupally <boddupally.venkatesh.git@gmail.com>
2018-10-29 21:26:21 +0530

    implemented roll functionality

--- app.js
+++ app.js
@@ -8,3 +8,59 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+var totalScore;
+var roundScore, activePlayer;
+
+totalScore = [0, 0];
+roundScore = 0;
+activePlayer = 0;
+
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+
+
+document.querySelector('.dice').style.display = 'none';
+
+
+document.querySelector('.btn-roll').addEventListener('click', function(){
+    //1. randome number
+    var dice = Math.floor(Math.random() * 6) + 1;
+
+
+    //2. display result
+    var diceDom = document.querySelector('.dice');
+    diceDom.style.display = 'block';
+    diceDom.src = 'dice-' + dice + '.png';
+
+    //3. update round score if dice not 1
+    if(dice !==1 ){
+        roundScore = roundScore + dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    }else{
+
+        activePlayer ===0 ? activePlayer = 1 : activePlayer = 0;
+
+        roundScore = 0;
+        
+        document.querySelector('#current-0').textContent = '0';
+        document.querySelector('#current-1').textContent = '0';
+
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        // document.querySelector('."player-0-panel').classList.remove('active');
+        // document.querySelector('."player-1-panel').classList.add('active');
+
+        document.querySelector('.dice').style.display = 'none';
+    }
+})
+
+
+// dice = Math.floor(Math.random() * 6) + 1;
+
+// document.querySelector('#current-0').textContent = dice;
+// console.log(dice);
